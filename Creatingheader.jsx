965a84d8064cb63795e8e5b0009f4cda5f921600
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2023-03-15 12:59:23 +0530

    Creating Header

---
+++ Creatingheader.jsx
@@ -0,0 +1,13 @@
+const App = ({title}) => (
+  <div className="header">{title}</div>
+);
+
+ReactDOM.render(
+	<App title="The GitHub Cards App" />,
+  mountNode,
+);
+
+// *** The React 18 way:
+// root.render(
+//   <App title="The GitHub Cards App" />,
+// );
